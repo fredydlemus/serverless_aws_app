65534fd8a02a1c239baa777a1665cbd657b858c2
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2025-05-11 10:52:33 -0600

    apply fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 module "orders_table" {
-    source = "./modules/dynamodb"
-    name                    = "orders"
-    partition_key          = "order_id"
-    billing_mode           = "PAY_PER_REQUEST"
-    point_in_time_recovery = true
-    server_side_encryption = true
-    
-    environment = terraform.workspace
-    project     = var.project
-    owner       = var.owner
+  source                 = "./modules/dynamodb"
+  name                   = "orders"
+  partition_key          = "order_id"
+  billing_mode           = "PAY_PER_REQUEST"
+  point_in_time_recovery = true
+  server_side_encryption = true
+
+  environment = terraform.workspace
+  project     = var.project
+  owner       = var.owner
 }
